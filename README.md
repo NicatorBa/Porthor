05c23a23948529290619b815a31244fa41bffc49
NicatorBa <daniel@bartschinski.info>
2017-05-20 15:26:27 +0200

    Update readme, add builder extension, update nuget package

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Porthor
-API gateway middleware for Asp.Net Core
+**What's Porthor?**
+
+Porthor is an API gateway middleware for Asp.Net Core. It offers an easy to use way to register APIs from other sources to your application. You can also protect your API by using ASP.NET Core policy-based authorization.
+
+## Getting started
+
+- **Update your `.csproj` file** to reference `Porthor`package
+
+- **Configure Porthor service** in `Startup`:
+
+```csharp
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup()
+    {
+        // Add your configuration sources
+        _configuration = new ConfigurationBuilder()
+                                .Build();
+    }
+
+    public void ConfigureServices(IServiceCollection service)
+    {
+        // Register the Porthor services
+        services.AddPorthor(_configuration, options =>
+        {
+            // Your gateway configuration
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UsePorthor();
+    }
+}
+```
--- src/Porthor/PorthorBuilderExtension.cs
+++ src/Porthor/PorthorBuilderExtension.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Porthor;
+using Porthor.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Microsoft.AspNetCore.Builder
 {
@@ -18,6 +21,27 @@ namespace Microsoft.AspNetCore.Builder
             {
                 throw new ArgumentNullException(nameof(builder));
             }
+            return builder.UseMiddleware<PorthorMiddleware>();
+        }
+
+        /// <summary>
+        /// Adds a <see cref="PorthorMiddleware"/> to the specified <see cref="IApplicationBuilder"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+        /// <param name="resources">A collection of resources to initialize startup routes.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UsePorthor(this IApplicationBuilder builder, IEnumerable<Resource> resources)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (resources == null)
+            {
+                throw new ArgumentNullException(nameof(resources));
+            }
+            var router = (IPorthorRouter)builder.ApplicationServices.GetService(typeof(IPorthorRouter));
+            router.Initialize(resources).Wait();
             return builder.UseMiddleware<PorthorMiddleware>();
         }
     }
