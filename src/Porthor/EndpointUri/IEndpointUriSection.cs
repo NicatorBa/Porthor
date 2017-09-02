994713c07ab4ec17720ecc3a52d01a0ea20c2d4b
NicatorBa <daniel@bartschinski.info>
2017-09-02 14:29:31 +0200

    add documentation comments for endpoint uri

--- src/Porthor/EndpointUri/IEndpointUriSection.cs
+++ src/Porthor/EndpointUri/IEndpointUriSection.cs
@@ -2,8 +2,16 @@
 
 namespace Porthor.EndpointUri
 {
+    /// <summary>
+    /// Provides an abstraction of an endpoint uri section.
+    /// </summary>
     public interface IEndpointUriSection
     {
+        /// <summary>
+        /// Creates a section for the endpoint uri.
+        /// </summary>
+        /// <param name="values">Route values of current route.</param>
+        /// <returns>Current enpoint uri section.</returns>
         string CreateSection(RouteValueDictionary values);
     }
 }
