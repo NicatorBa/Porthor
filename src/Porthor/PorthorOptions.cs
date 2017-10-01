0e5242634793931f15fc78aa1dfdd3887706e550
NicatorBa <daniel@bartschinski.info>
2017-10-01 22:53:23 +0200

    Documentation for options

--- src/Porthor/PorthorOptions.cs
+++ src/Porthor/PorthorOptions.cs
@@ -1,11 +1,23 @@
 ﻿namespace Porthor
 {
+    /// <summary>
+    /// Configuration options for <see cref="Microsoft.AspNetCore.Builder.PorthorMiddleware"/>.
+    /// </summary>
     public class PorthorOptions
     {
+        /// <summary>
+        /// Indicator that indicates whether the query string is to be validated.
+        /// </summary>
         public bool QueryStringValidationEnabled { get; set; }
 
+        /// <summary>
+        /// Security configuration options.
+        /// </summary>
         public SecurityOptions Security { get; private set; } = new SecurityOptions();
 
+        /// <summary>
+        /// Request content configuration options.
+        /// </summary>
         public ContentOptions Content { get; private set; } = new ContentOptions();
     }
 }
--- src/Porthor/SecurityOptions.cs
+++ src/Porthor/SecurityOptions.cs
@@ -1,9 +1,18 @@
 ﻿namespace Porthor
 {
+    /// <summary>
+    /// Security configuration options for <see cref="Microsoft.AspNetCore.Builder.PorthorMiddleware"/>.
+    /// </summary>
     public class SecurityOptions
     {
+        /// <summary>
+        /// Flag indicating whether authentication validations should be used.
+        /// </summary>
         public bool AuthenticationValidationEnabled { get; set; }
 
+        /// <summary>
+        /// Flag indicating whether authorization validations should be used.
+        /// </summary>
         public bool AuthorizationValidationEnabled { get; set; }
     }
 }
